c4dca2a0de004207bf8ec2bdcef4324aafb45fc6
RamonOrtisz <www.gamesetutoriaiss@gmail.com>
2024-03-04 23:27:31 -0300

    adicionando um comentario

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,23 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
+import {  fireEvent, render, screen } from '@testing-library/react';
 import PostComment from '.';
+import App from '../../App';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+        const { debug } = render(<PostComment/>);
+        debug()
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    test('Deve adicionar um novo comentario', () => {
+        const { debug } = render(<App />)
+        fireEvent.change(screen.getByTestId('campo-tarefa'), {
+        target: {
+            value:'comenta olá'
+        } 
+        })
+        fireEvent.click(screen.getByTestId('btn-cadastrar'))
+        debug()
+        expect(screen.getByText('comenta olá')).toBeInTheDocument()
+    })
 });
